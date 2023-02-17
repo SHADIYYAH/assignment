479dc8d9b4df945507eeae13e790862c27a99659
unknown <ajanihalimat2020@gmail.com>
2023-02-17 09:09:13 +0100

    Message

---
+++ zulfah_assignment.js
@@ -0,0 +1,54 @@
+function tellFortune(jobTitle, geographicLocation, partnerName, 
+numberOfchildren){
+  let Fortune = "You will be an " +jobTitle +" in " +geographicLocation + " and married to " +partnerName +" with" +numberOfchildren
+    
+  return Fortune
+}
+  console.log(tellFortune( "Engineer", "US", " Halimah", " three children"))
+  console.log(tellFortune( "accountant", "France", " Mbappe", " two children"))
+  console.log(tellFortune( "Actress", "Korea", " Lee Min Ho", " one child"))
+
+
+  function calculateDogAge(puppyAge){
+    let age = "Your doggie is " +puppyAge + " Years old in dog years"
+    return age
+  }
+    console.log(calculateDogAge( 7))
+    console.log(calculateDogAge( 20))
+    console.log(calculateDogAge( 15))
+
+
+
+function calculateSupply(age, amountPerDay){
+    let feedback = "You will need " +amountPerDay + " to last you until the ripe old age of " +age
+  return feedback
+  }
+  console.log(calculateSupply("50", "20"))
+  console.log(calculateSupply("100", "25"))
+  console.log(calculateSupply("150", "30"))
+
+
+  
+ function calcCircumference(radius){
+    return "The circumference is " +"2*r"
+   }
+    console.log(calcCircumference("r"))
+   function calArea(radius){
+        return "The area is " +"r*r"
+     }
+     console.log(calArea("r*r"))
+
+
+
+function celciusToFarenheit(celcius){
+    return celcius +" is"  +" 68F"
+  }
+  console.log(celciusToFarenheit("20C"))
+  
+  function farenheitToCelcius(farenheit){
+   return farenheit + " is" +" 482F"
+    };
+  console.log (farenheitToCelcius("250C"))
+
+
+
